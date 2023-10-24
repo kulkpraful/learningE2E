17ef48f8fe52ab593fdf48d3cf0979aca58f5527
kulkpraful <prafkulk@gmail.com>
2023-10-24 11:36:08 +0530

    pushing new changes
    pushing new changes

--- userservice/src/main/java/com/learning/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/learning/userservice/UserserviceApplication.java
@@ -1,11 +1,14 @@
 package com.learning.userservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.learning.userservice.repository")
+//@EnableJpaRepositories(basePackages = "com.learning.userservice.repository")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
--- userservice/src/main/java/com/learning/userservice/controller/UserController.java
+++ userservice/src/main/java/com/learning/userservice/controller/UserController.java
@@ -1,27 +1,28 @@
 package com.learning.userservice.controller;
 
-import com.learning.userservice.entity.User;
-import com.learning.userservice.repository.UserRepository;
 import com.learning.userservice.userdetails.UserDetails;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
     @GetMapping("/{userId}")
-    public  String getUserById(@PathVariable Integer userId){
+    public ResponseEntity<String> getUserById(@PathVariable(name = "userId", required = false) Integer userId){
         UserDetails userDetail = new UserDetails();
         List<String> userDetails = userDetail.getUserDetails(userId);
+        String userName  = userDetails.get(0);
+        String emailId = userDetails.get(1);
         if(!userDetails.isEmpty()){
-            return "User details: "+userDetails.get(0)+ " and "+userDetails.get(1);
+            return  ResponseEntity.ok("User details: " + userName + " and " + emailId);
         }else{
-            return "No record found for userId: "+userId;
+            return ResponseEntity.badRequest().body("No record found for userId: " + userId);
         }
 
     }
